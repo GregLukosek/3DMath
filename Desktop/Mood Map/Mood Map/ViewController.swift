0630f46f168a6f2506f07c06976e88b8d8918524
GregLukosek <greg@the-app-developers.co.uk>
2015-02-23 11:33:07 +0000

    first commit

---
+++ Desktop/Mood Map/Mood Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mood Map
+//
+//  Created by Greg Lukosek on 31/10/2014.
+//  Copyright (c) 2014 Greg Lukosek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
